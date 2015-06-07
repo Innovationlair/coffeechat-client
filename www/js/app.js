7c263b587560b77197a5f0b0ac50fb584edfaabd
Alexander Marinov <sasho_pleven@abv.bg>
2015-06-07 16:44:30 +0300

    Add new ionic project

--- www/js/app.js
+++ www/js/app.js
@@ -1,40 +1,73 @@
-angular.module("coffeechat", ['coffeechat.services.token', 'coffeechat.services.socket', 'coffeechat.controllers.networkController', 'coffeechat.controllers.networksListController', 'coffeechat.controllers.chatController', 'ionic']).config(function($stateProvider, $urlRouterProvider) {
-  console.log('CoffeeChat configured!');
-  return $stateProvider.state('network', {
-    url: '/network',
-    templateUrl: 'templates/members-list.html',
-    controller: 'NetworkController'
-  }).state('networks', {
-    url: '/networks',
-    templateUrl: 'templates/networks-list.html',
-    controller: 'NetworksListController'
-  }).state('chat', {
-    url: '/chatwith/:memberId',
-    templateUrl: 'templates/chat.html',
-    controller: 'ChatController'
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleLightContent();
+    }
   });
-}).run(function($ionicPlatform, $window, TokenService, $state, socket) {
-  return $ionicPlatform.ready(function() {
-    console.log('Device ready');
-    return TokenService.setup_token().then(function(token) {
-      console.log(token);
-      socket.on('connect', function() {
-        return socket.emit('register', token);
-      });
-      socket.on('message', function(data) {
-        return console.log('Received:', data);
-      });
-      switch ($window.navigator.network.connection.type) {
-        case Connection.WIFI:
-          return $state.go('network');
-        case Connection.CELL_2G:
-        case Connection.CELL_3G:
-        case Connection.CELL_4G:
-        case Connection.CELL:
-          return $state.go('networks');
-        default:
-          return console.log('No internet');
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: "/tab",
+    abstract: true,
+    templateUrl: "templates/tabs.html"
+  })
+
+  // Each tab has its own nav history stack:
+
+  .state('tab.chats', {
+      url: '/chats',
+      views: {
+        '': {
+          templateUrl: 'templates/tab-chats.html',
+          controller: 'ChatsCtrl'
+        }
       }
-    });
+    })
+    .state('tab.chat-detail', {
+      url: '/chats/:chatId',
+      views: {
+        '': {
+          templateUrl: 'templates/chat-detail.html',
+          controller: 'ChatDetailCtrl'
+        }
+      }
+    })
+
+  .state('tab.account', {
+    url: '/account',
+    views: {
+      '': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
+      }
+    }
   });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/chats');
+
 });
